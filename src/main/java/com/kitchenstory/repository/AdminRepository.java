5d048b94ac80180e8a769cac47e1e10a7571ebef
Nirosh <nprakash@adobe.com>
2021-10-30 11:34:03 +0530

    Initial commit

---
+++ src/main/java/com/kitchenstory/repository/AdminRepository.java
@@ -0,0 +1,27 @@
+package com.kitchenstory.repository;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kitchenstory.entity.Admin;
+
+@Repository
+@Transactional
+public class AdminRepository {
+
+	@Autowired
+	EntityManager em;
+
+	public Admin findById(String id) {
+		Admin admin = em.find(Admin.class, id);
+		return admin;
+	}
+
+	public void updatePassword(Admin admin) {
+		em.merge(admin);
+	}
+
+}
